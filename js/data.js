aef4dddf2be01725d4f59dccf457798d824b555e
Yomakota <teoperitore@outlook.com>
2022-06-21 21:50:06 +0200

    add bonus-2

--- js/data.js
+++ js/data.js
@@ -11,7 +11,7 @@
 // 1 - modificare la struttura dati fornita e valorizzare la proprietà "color" in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo "#" seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
 // 2 - popolare le options della select della milestone 3 dinamicamente.
 
-data = [
+const allIcons = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -126,18 +126,113 @@ data = [
 	}
 ];
 
-//------------
-//COMMENTS
-//------------
-//funzione per creare le card
-//con un template per le card dinamico con le proprietà dell'oggetto
 
-// ciclo for per stampare in pagina tutte le card
+// GENERATORE COLORE RANDOM PER OGNI ELEMENTO DELLE CARDS 
+allIcons.forEach((element) => {
+	element.color = generateColor();
+});
 
-// salvo in una variabile con ciclo filter i diversi elementi per type
-//ES.= data.filter(element) return element.type == user/vegetable/animal
+// Richiamo il mio elemento HTML a cui voglio inserire tutte le mie cards
+const iconContainer = document.querySelector('.js-all-card');
 
-// creata una select in html attribuisco le variabili filtrate ad ogni value
+// FUNCTION PER POPOLARE IL DOM
+printCards(allIcons, iconContainer);
 
-// e con un ciclo for o forEach stampo in pagina con la funzione i differenti elementi dipende la condizione
-// ES.= user/animal/vegetables.forEach((element) => { });
+
+// ----------------------
+//     EVENT CHANGE
+// ----------------------
+// CATEGORIZZO LE CARDS DA STAMPARE IN BASE ALLA SCELTA DELL'UTENTE 
+const selectCategory = document.querySelector('#category');
+// Reset dei valori a All per ogni volta che l'utente fa refresh della pagina
+selectCategory.value = 'all';
+
+selectCategory.addEventListener('change',
+	function () {
+
+		// Rendo leggibile la scelta dell'utente
+		const userValue = this.value;
+		// Svuoto il popolamento delle pagine ogni volta che l'utente cambia la sua categoria
+		iconContainer.innerHTML = '';
+
+		// Se nei dati dell'oggetto non e' compreso il type ALL allora 
+		if (userValue !== 'all') {
+			const filteredIcons = allIcons.filter((element) => {
+				return element.type === userValue;
+			});
+
+			// Popolazione DOM
+			printCards(filteredIcons, iconContainer);
+		} else {
+			// Popolazione DOM
+			printCards(allIcons, iconContainer);
+		}
+	}
+);
+
+
+// --------------------
+// 		FUNCTION
+// --------------------
+// Popolazione DOM
+function printCards(array, container) {
+	array.forEach((element) => {
+		const { name, prefix, type, family, color } = element;
+
+		const newCard = `
+		<div class="card flex" style="color: ${color}">
+			<i class="${family} ${prefix}${name}"></i>
+			<span>${name}</span>
+		</div>
+		`
+		container.innerHTML += newCard;
+	});
+};
+
+// Generatore colore casuale
+function generateColor() {
+	let color = '#';
+
+	// Generiamo una stringa che sia della stessa lunghezza di: #1b2b3a (esadecimale) 
+	// Variante a stringa con tutti i numeri a nostra disposizione
+	const symbols = '0123456789abcdef';
+
+	for (let i = 0; i < 6; i++) {
+		const randomIndex = getRndInteger(0, symbols.length - 1);
+
+		const randomSymbol = symbols[randomIndex];
+
+		color += randomSymbol;
+	}
+	return color;
+};
+
+
+// Generatore numero random
+function getRndInteger(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+
+//BONUS
+// 2 - popolare le options della select della milestone 3 dinamicamente.
+let optionValue = 'all';
+
+allIcons.forEach((element) => {
+
+	if (optionValue == 'all') {
+
+		const opt = `<option value="${optionValue}">All</option>`;
+		optionValue = false;
+
+		selectCategory.innerHTML += opt;
+
+	} else if (optionValue !== element.type) {
+		
+		const opt = `<option value="${element.type}">${element.type}</option>`;
+		optionValue = element.type;
+
+		selectCategory.innerHTML += opt;
+	}
+
+});
